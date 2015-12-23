51a3fd39da14f445a46471e5cd01f0e685b36a98
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2015-12-23 20:24:47 +0000

    ds

--- CapValue/settings.py
+++ CapValue/settings.py
@@ -1,14 +1,3 @@
-"""
-Django settings for CapValue project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
