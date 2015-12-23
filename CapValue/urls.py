d2cf9192ec2c261427ead0dbe3f7057181cfa0c8
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2015-12-23 21:14:09 +0000

    ds

--- CapValue/urls.py
+++ CapValue/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
+from CapValue.views import IndexView
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^api/v1/', include(router.urls)),
+
+                       url('^.*$', IndexView.as_view(), name='index'),
+                       )
