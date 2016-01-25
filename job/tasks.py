8d47976810f0f45db782d731f473d217606158ae
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2016-01-25 23:08:27 +0000

    Whatever.

--- job/tasks.py
+++ job/tasks.py
@@ -7,7 +7,7 @@ from CapValue.celery_settings import app
 logger = get_task_logger(__name__)
 
 
-@app.task(name='report_task')
+@app.task(name='report_task', bind=True)
 def reportTask(self, link):
     print('report_task')
     logger.info('Reported Email')
