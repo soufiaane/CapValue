71d587996a1da67b6fb392a2cc7611e09d642d89
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2016-01-25 16:50:49 +0000

    dirty hack, have a better idea ?

--- CapValue/wsgi.py
+++ CapValue/wsgi.py
@@ -1,5 +1,6 @@
 import django.core.handlers.wsgi
-import os
+import os,sys
 
+sys.path.append('/var/www/cvc.ma')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CapValue.settings")
 application = django.core.handlers.wsgi.WSGIHandler()
