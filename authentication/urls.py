2e06f2fa5e4764ce9426d02c290916b961113d6c
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2016-01-25 17:52:07 +0000

    Too lazy to write descriptive message

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^auth/login/$', LoginView.as_view(), name='login'),
-    url(r'^auth/logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^auth/login$', LoginView.as_view(), name='login'),
+    url(r'^auth/logout$', LogoutView.as_view(), name='logout'),
 ]
