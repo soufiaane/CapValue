6650d1edd88d16e1add07396b0104ef0768269c0
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2016-02-09 23:54:51 +0000

    debugo

--- ttt.py
+++ ttt.py
@@ -4,11 +4,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import time
 
-
 while True:
 
     # region Settings
-    ACTIONS = 'RS'.split(',')#RS,NS
+    ACTIONS = 'SS,AC,CL,FM'.split(',')  # RS,NS
     PROXY = "67.21.35.254:8674"
     Keyword = 'Validate'
     chrome_options = webdriver.ChromeOptions()
@@ -17,7 +16,8 @@ while True:
     # browser = webdriver.PhantomJS(executable_path="phantomjs.exe", service_args=service_args)
     # browser = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
     browser = webdriver.Chrome(executable_path="chromedriver")
-    # browser.maximize_window()
+    browser.maximize_window()
+
 
     def look_for_pub():
         try:
@@ -29,22 +29,39 @@ while True:
         except Exception:
             print('')
 
+
+    def look_for_unblock():
+        try:
+            btn_unblock = browser.find_element_by_xpath('//*[@id="notificationContainer"]/div/div/div/div/div[2]/button')
+            handles_before = browser.window_handles
+            btn_unblock.click()
+            WebDriverWait(browser, 1).until(lambda browser: len(handles_before) != len(browser.window_handles))
+            print('# Unblock Clicked')
+        except NoSuchElementException:
+            pass
+
+
     def waiit():
         try:
+            look_for_pub()
+            look_for_unblock()
             while browser.execute_script('return document.readyState;') != 'complete':
                 look_for_pub()
+                look_for_unblock()
         except:
             pass
+
+
     # endregion
 
     try:
 
         # region Connection
-        email = 'KatielSierenb2777@hotmail.com'
-        pswd = 'cmxfisi51'
+        email = 'CristinpPaysong3493@hotmail.com'
+        pswd = 'iiwosig18'
         link = 'http://www.hotmail.com'
         browser.get(link)
-        if  'ERR_PROXY_CONNECTION_FAILED' in str(browser.page_source):
+        if 'ERR_PROXY_CONNECTION_FAILED' in str(browser.page_source):
             print('problem PROXY')
             break
         default_window = browser.window_handles[0]
@@ -77,6 +94,7 @@ while True:
 
         # region Spam Actions
         try:
+            waiit()
             spam_count = int(browser.find_elements_by_css_selector('span.count')[2].text)
         except Exception:
             spam_count = 0
@@ -85,73 +103,90 @@ while True:
         if spam_count > 0:
             browser.get(junk_url)
             waiit()
-            look_for_pub()
-            if 'RS' in ACTIONS: # Mark Spam as read
+            if 'RS' in ACTIONS:  # Mark Spam as read
                 while (not browser.find_element_by_css_selector('div.NextPageDisabled').is_displayed()) or spam_count > 0:
+                    waiit()
                     browser.find_element_by_id('msgChkAll').click()
                     time.sleep(1)
-                    browser.find_element_by_xpath('//*[@title=" Autres commandes"]').click()
+                    waiit()
+                    try:
+                        browser.find_element_by_xpath('//*[@title=" Autres commandes"]').click()
+                    except NoSuchElementException:
+                        try:
+                            browser.find_element_by_xpath('//*[@title="More commands"]').click()
+                            pass
+                        except NoSuchElementException:
+                            pass
                     time.sleep(1)
+                    waiit()
                     browser.find_element_by_id('MarkAsRead').click()
+                    waiit()
                     time.sleep(1)
-                    if ('NS' in ACTIONS): # Not SPAM
+                    if ('NS' in ACTIONS):  # Not SPAM
+                        waiit()
                         browser.find_element_by_id('MarkAsNotJunk').click()
                         waiit()
-                        look_for_pub()
                         browser.get(junk_url)
                     else:
+                        waiit()
                         browser.find_element_by_id('nextPageLink').click()
                     try:
+                        waiit()
                         spam_count = int(browser.find_elements_by_css_selector('span.count')[2].text)
                         print('Total Spam: ' + str(spam_count))
                     except Exception:
                         spam_count = 0
-            if ('NS' in ACTIONS) and ('RS' not in ACTIONS): # Not SPAM
+            if ('NS' in ACTIONS) and ('RS' not in ACTIONS):  # Not SPAM
                 while (not browser.find_element_by_css_selector('div.NextPageDisabled').is_displayed()) or spam_count > 0:
+                    waiit()
                     browser.find_element_by_id('msgChkAll').click()
                     time.sleep(1)
+                    waiit()
                     browser.find_element_by_id('MarkAsNotJunk').click()
                     waiit()
-                    look_for_pub()
                     browser.get(junk_url)
                     waiit()
-                    look_for_pub()
                     try:
                         spam_count = int(browser.find_elements_by_css_selector('span.count')[2].text)
                         print('Total Spam: ' + str(spam_count))
                     except Exception:
                         spam_count = 0
-            elif ('SS' in ACTIONS) and ('RS' not in ACTIONS) and ('NS' not in ACTIONS): # Mark SPAM as Safe
+            elif ('SS' in ACTIONS) and ('RS' not in ACTIONS) and ('NS' not in ACTIONS):  # Mark SPAM as Safe
                 try:
+                    waiit()
                     email_list = browser.find_element_by_css_selector('ul.mailList')
                     emails = email_list.find_elements_by_tag_name('li')
                     emails[0].click()
                     waiit()
-                    look_for_pub()
-                except Exception:
+                except Exception as ex:
+                    print(type(ex))
                     pass
                 while spam_count > 0:
                     # Mark Safe
                     try:
+                        waiit()
                         safe_link = browser.find_element_by_css_selector('a.sfUnjunkItems')
                         safe_link.click()
-                    except:
+                        waiit()
+                    except Exception as ex:
+                        print(type(ex))
                         pass
 
                     try:
+                        waiit()
                         spam_count = int(browser.find_elements_by_css_selector('span.count')[2].text)
                         print('Total Spam: ' + str(spam_count))
                     except Exception:
                         spam_count = 0
 
                     try:
+                        waiit()
                         email_list = browser.find_element_by_css_selector('ul.mailList')
                         emails = email_list.find_elements_by_tag_name('li')
                         emails[0].click()
                         waiit()
-                        look_for_pub()
-                    except Exception:
-                        pass
+                    except Exception as ex:
+                        print(type(ex))
         else:
             print('Nothing to do here : ')
         # endregion
@@ -160,19 +195,15 @@ while True:
 
         if not str(browser.current_url).endswith('inbox'): browser.get(inbox_url)
         waiit()
-        look_for_pub()
         keywork_link = str(browser.current_url)[:str(browser.current_url).index('.com')] + '.com/?fid=flsearch&srch=1&skws=' + Keyword + '&sdr=4&satt=0'
         browser.get(keywork_link)
         waiit()
-        look_for_pub()
 
         # region Mark inbox as Read
-        if 'RI' in ACTIONS: # Mark inbox as Read
+        if 'RI' in ACTIONS:  # Mark inbox as Read
             new_link = str(browser.current_url).replace('&sdr=4&satt=0', '&scat=1&sdr=4&satt=0')
             browser.get(new_link)
             waiit()
-            look_for_pub()
-
             try:
                 browser.find_element_by_id('NoMsgs')
                 no_results = True
@@ -181,58 +212,91 @@ while True:
 
             while not no_results:
                 time.sleep(1)
+                waiit()
                 browser.find_element_by_id('msgChkAll').click()
                 time.sleep(1)
-                browser.find_element_by_xpath('//*[@title=" Autres commandes"]').click()
+                waiit()
+                try:
+                    browser.find_element_by_xpath('//*[@title=" Autres commandes"]').click()
+                    waiit()
+                except NoSuchElementException:
+                    try:
+                        browser.find_element_by_xpath('//*[@title="More commands"]').click()
+                        waiit()
+                        pass
+                    except NoSuchElementException:
+                        pass
                 time.sleep(1)
+                waiit()
                 browser.find_element_by_id('MarkAsRead').click()
                 time.sleep(1)
+                waiit()
                 browser.get(new_link)
                 waiit()
-                look_for_pub()
                 try:
                     browser.find_element_by_id('NoMsgs')
                     no_results = True
                 except NoSuchElementException:
                     no_results = False
-                    continue
+                    pass
         # endregion
 
         # region Add contact Inbox / click Links
-        elif ('AC' in ACTIONS) or ('CL' in ACTIONS):  # Add contact Inbox
+        if ('AC' in ACTIONS) or ('CL' in ACTIONS):  # Add contact Inbox
             try:
                 browser.get(keywork_link)
                 waiit()
-                look_for_pub()
                 emails = browser.find_elements_by_css_selector('li.c-MessageRow')
                 waiit()
-                look_for_pub()
                 emails[0].find_elements_by_tag_name('span')[1].click()
                 waiit()
-                look_for_pub()
-                next_btn = WebDriverWait(browser, 10).until(lambda browser : browser.find_element_by_css_selector('a.rmNext').find_element_by_tag_name('img'))
+                next_btn = WebDriverWait(browser, 10).until(lambda browser: browser.find_element_by_css_selector('a.rmNext').find_element_by_tag_name('img'))
+                waiit()
                 next_btn_attributes = next_btn.get_attribute('class')
+                waiit()
                 last_msg = True if str(next_btn_attributes).endswith('_d') else False
                 waiit()
-                look_for_pub()
-
                 try:
                     while not last_msg:
                         try:
                             # region Add Contact
                             if 'AC' in ACTIONS:
                                 try:
+                                    waiit()
                                     add_contact_link = browser.find_element_by_css_selector('a.AddContact')
                                     add_contact_link.click()
-                                    time.sleep(0.5)
+                                    time.sleep(1)
+                                    waiit()
                                     print('Contact Added')
                                 except NoSuchElementException:
                                     print('Contact Already Exist')
                                     pass
                             # endregion
 
+                            # region Trust email Content
+                            try:
+                                browser.find_element_by_css_selector('a.sfMarkAsSafe').click()
+                                time.sleep(1)
+                                waiit()
+                            except NoSuchElementException:
+                                pass
+                            # endregion
+
+                            # region Flag Mail
+                            if 'FM' in ACTIONS:
+                                waiit()
+                                try:
+                                    flag = browser.find_elements_by_css_selector('div.MessageHeaderItem')[3].find_element_by_css_selector('img.ia_i_p_1')
+                                    flag.click()
+                                    waiit()
+                                    time.sleep(0.5)
+                                except NoSuchElementException:
+                                    pass
+                            # endregion
+
                             # region Click Links
                             if 'CL' in ACTIONS:
+                                waiit()
                                 body1 = browser.find_element_by_css_selector('div.readMsgBody')
                                 body = body1.find_elements_by_tag_name('div')
                                 try:
@@ -240,72 +304,82 @@ while True:
                                 except Exception:
                                     lnk = None
                                 waiit()
-                                look_for_pub()
                                 if lnk is not None:
+                                    waiit()
                                     lnk.click()
                                     waiit()
-                                    browser.switch_to.window(browser.window_handles[1])
-                                    print(browser.title)
-                                    browser.close()
-                                    browser.switch_to.window(browser.window_handles[0])
-                                    waiit()
-                                    look_for_pub()
+                                    if len(browser.window_handles) > 0:
+                                        browser.switch_to.window(browser.window_handles[1])
+                                        waiit()
+                                        browser.close()
+                                        waiit()
+                                        browser.switch_to.window(browser.window_handles[0])
+                                        waiit()
+                                    else:
+                                        time.sleep(1)
+                                        lnk.click()
+                                        time.sleep(1)
+                                        waiit()
+                                        if len(browser.window_handles) > 0:
+                                            browser.switch_to.window(browser.window_handles[1])
+                                            waiit()
+                                            browser.close()
+                                            waiit()
+                                            browser.switch_to.window(browser.window_handles[0])
+                                            waiit()
                             # endregion
-
-                            waiit()
-                            look_for_pub()
                             bod = browser.find_elements_by_tag_name('body')[0]
                             bod.send_keys(Keys.CONTROL + ';')
                             time.sleep(1)
                             waiit()
-                            look_for_pub()
-                        except NoSuchElementException as nse:
-                            print(nse)
+                        except NoSuchElementException as ex:
+                            print(type(ex))
                             continue
-                        except StaleElementReferenceException as se:
-                            print(se)
+                        except StaleElementReferenceException as ex:
+                            print(type(ex))
                             continue
                         finally:
                             time.sleep(1)
+                            waiit()
                             try:
                                 next_btn = browser.find_element_by_css_selector('a.rmNext').find_element_by_tag_name('img')
-                            except Exception as x:
-                                print(type(x))
+                            except Exception as ex:
+                                print(type(ex))
                                 next_btn = None
                             next_btn_attributes = next_btn.get_attribute('class') if next_btn else ''
                             last_msg = True if str(next_btn_attributes).endswith('_d') else False
 
-                except NoSuchElementException as nse:
-                    print(nse)
+                except NoSuchElementException as ex:
+                    print(type(ex))
                     continue
-                except StaleElementReferenceException as se:
-                    print(se)
+                except StaleElementReferenceException as ex:
+                    print(type(ex))
                     continue
 
-            except NoSuchElementException as nse:
-                print(nse)
+            except NoSuchElementException as ex:
+                print(type(ex))
                 continue
-            except StaleElementReferenceException as se:
-                print(se)
+            except StaleElementReferenceException as ex:
+                print(type(ex))
                 continue
-            except Exception as exc:
-                print(exc)
+            except Exception as ex:
+                print(type(ex))
                 continue
         # endregion
 
-        #region Flag mail
-        elif 'FM' in ACTIONS: # Flag mail
+        # region Flag mail
+        if 'FM' in ACTIONS:  # Flag mail
+            waiit()
             browser.get(keywork_link)
             waiit()
-            look_for_pub()
             last_page = browser.find_element_by_css_selector('div.NextPageDisabled').is_displayed()
             last_page_checked = False
             while (not last_page) and (not last_page_checked):
+                waiit()
                 messages = browser.find_element_by_css_selector('ul.mailList').find_elements_by_tag_name('li')
                 for msg in messages:
                     try:
                         waiit()
-                        look_for_pub()
                         flag = msg.find_element_by_css_selector('img.ia_i_p_1')
                         flag.click()
                         time.sleep(1)
@@ -313,20 +387,19 @@ while True:
                     except NoSuchElementException:
                         pass
                 last_page_checked = last_page
-                browser.find_element_by_id('nextPageLink').click()
                 waiit()
+                browser.find_element_by_id('nextPageLink').click()
                 time.sleep(1)
+                waiit()
                 last_page = browser.find_element_by_css_selector('div.NextPageDisabled').is_displayed()
         # endregion
 
         # endregion
 
         print('Now What ??')
-        browser.get(keywork_link)
         waiit()
         look_for_pub()
-
-    except Exception as exc:
-        print(type(exc))
+    except Exception as ex:
+        print(type(ex))
     finally:
         browser.quit()
