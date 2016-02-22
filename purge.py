78031d9c02d5668dc1934ae7afa265571aa9535d
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2016-02-22 18:56:29 +0000

    Jefferson broke the regex, lame

--- purge.py
+++ purge.py
@@ -8,4 +8,4 @@ app = Celery('CapValue', broker='amqp://soufiaane:C@pV@lue2016@cvc.ma/cvcHost')
 
 @app.task(name='report_hotmail', bind=True, max_retries=3, default_retry_delay=1)
 def report_hotmail(self, job, email):
-    pass
+    return True
