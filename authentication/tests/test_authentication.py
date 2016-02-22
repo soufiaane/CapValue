6d7989def57574d8170f19474b14930152286da7
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2016-02-22 19:27:18 +0000

    Jefferson broke the regex, lame

---
+++ authentication/tests/test_authentication.py
@@ -0,0 +1,22 @@
+from authentication.models import Account
+from django.test import TestCase
+
+
+class AccountTests(TestCase):
+
+    def test_est_recent_avec_futur_article(self):
+        """
+
+        Vérifie si la méthode est_recent d'un Article ne
+
+        renvoie pas True si l'Article a sa date de publication
+
+        dans le futur.
+
+        """
+
+        admin = Account.objects.latest('created_at')
+
+        # Il n'y a pas besoin de remplir tous les champs, ni de sauvegarder
+
+        self.assertEqual(admin.first_name, False)
