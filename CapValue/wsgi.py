a301d89ae15856271eae4ba0d22a62aa13e22338
Soufane MGHANEN <mgh.soufiane@gmail.com>
2016-02-23 10:20:03 +0000

    better grepping

--- CapValue/wsgi.py
+++ CapValue/wsgi.py
@@ -8,6 +8,3 @@ application = DjangoWhiteNoise(application)
 # sys.path.append('/var/www/cvc.ma')
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CapValue.settings")
 # application = django.core.handlers.wsgi.WSGIHandler()
-
-
-
