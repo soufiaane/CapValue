d15e1c5e14aae7d801897689e932d7890db2eb40
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2016-02-23 20:53:10 +0000

    lots and lots of changes

--- CapValue/wsgi.py
+++ CapValue/wsgi.py
@@ -1,7 +1,8 @@
-from configurations.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 import os
 
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CapValue.settings")
+from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
