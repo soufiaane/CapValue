04950938450161e000f3471408f157a372a2ab98
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2016-03-31 20:07:16 +0100

    forgot to save that file

--- CapValue/wsgi.py
+++ CapValue/wsgi.py
@@ -1,7 +1,7 @@
 import os, sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/vat/www/cvctools')
+sys.path.append('/var/www/cvctools')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CapValue.settings")
 
 application = get_wsgi_application()
